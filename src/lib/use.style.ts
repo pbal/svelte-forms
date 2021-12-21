5417112cb09aec88a9302dd8bb29c5906361fb81
INFOR\pbal <pukhraj.bal@infor.com>
2021-12-21 10:20:08 -0700

    add ids dirty

--- src/lib/use.style.ts
+++ src/lib/use.style.ts
@@ -1,10 +1,17 @@
 export function style(
 	node: HTMLElement,
-	{ field = null, valid = 'valid', invalid = 'invalid', dirty = 'dirty' } = {}
+	{ field = null, valid = 'valid', invalid = 'invalid', dirty = 'dirty',
+		iconClass = null } = {}
 ) {
+
+	const dirtyElement = document.createElement('span');
+	dirtyElement.classList.add('icon-dirty');
+	dirtyElement.classList.add(iconClass);
+
 	const unsubscribe = field.subscribe((field) => {
 		if (field.dirty) {
 			node.classList.add(dirty);
+			node.parentNode.insertBefore(dirtyElement, node);
 			if (field.valid) {
 				node.classList.add(valid);
 				node.classList.remove(invalid);
@@ -13,6 +20,10 @@ export function style(
 				node.classList.remove(valid);
 			}
 		} else {
+			var dirtyNode = node.parentNode.querySelector('.icon-dirty');
+			if (dirtyNode) {
+				node.parentNode.removeChild(dirtyNode);
+			}
 			node.classList.remove(dirty);
 		}
 	});
